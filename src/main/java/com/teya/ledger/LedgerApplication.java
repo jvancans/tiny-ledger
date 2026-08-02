05a0dd331624b040065353f8c93f688f57115540
Janis Vancans <janis.vancans@live.com>
2026-08-02 12:58:09 +0300

    Init

---
+++ src/main/java/com/teya/ledger/LedgerApplication.java
@@ -0,0 +1,13 @@
+package com.teya.ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(LedgerApplication.class, args);
+	}
+
+}
