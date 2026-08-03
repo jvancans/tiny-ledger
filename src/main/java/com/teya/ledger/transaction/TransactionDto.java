36dc9ec668c41fd263b94d3e6e3ad2e4a039455c
Janis Vancans <janis.vancans@live.com>
2026-08-03 08:54:23 +0300

    chore: improve transaction details docs

--- src/main/java/com/teya/ledger/transaction/TransactionDto.java
+++ src/main/java/com/teya/ledger/transaction/TransactionDto.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 public record TransactionDto(
 		@Schema(type = "string", format = "uuid", example = "ce58d887-2a59-4dc1-a83a-0d74ea642a71")
 		UUID id,
-		@Schema(description = "Timestamp in milliseconds since Epoch of transaction", example = "1785703675904")
+		@Schema(description = "Timestamp of transaction in milliseconds since Epoch", example = "1785703675904")
 		long timestampMillis,
 		TransactionType type,
 		@Schema(description = "Amount in transaction", example = "100.00")
